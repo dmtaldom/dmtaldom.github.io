9e5ed3235dd9a46871e2f1416cacdd10844dd35f
dmtaldom <krutovski@gmail.com>
2023-06-07 21:54:56 +0300

    adaptive 07.06

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu__list--active');
+})
 const swiper = new Swiper(".swiper", {
   effect: "fade",
   pagination: {
